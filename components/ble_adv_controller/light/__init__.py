1cd4f3700951ec81cbc0fd90e119153b710a54e0
Peter Sarkozi <xmisterhu@gmail.com>
2025-09-01 17:50:29 +0200

    ESPHome 2025.7 #2

--- components/ble_adv_controller/light/__init__.py
+++ components/ble_adv_controller/light/__init__.py
@@ -109,7 +109,7 @@ CONFIG_SCHEMA = cv.All(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await entity_base_code_gen(var, config)
+    await entity_base_code_gen(var, config, "light")
     cg.add(cg.App.register_light(var))
     await light.setup_light_core_(var, var, config)
     if config[CONF_TYPE] == "onoff":
